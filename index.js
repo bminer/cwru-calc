a2b5eb65b26964ef571f25d602f4fb7f60610103
jsghines <jsghines98@gmail.com>
2018-03-14 21:09:59 -0400

    Bill is inefficient

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
 	$('#plus').on('click', function() {
 		var num1 = $('#num1').val();
 		var num2 = $('#num2').val();
-		var result = parseInt(num1) + parseInt(num2);
+		var result = +(num1) + +(num2);
 		$('#result').text(result)
 	});
 	
 	$('#minus').on('click', function() {
 		var num1 = $('#num1').val();
 		var num2 = $('#num2').val();
-		var result = parseInt(num1) - parseInt(num2);
+		var result = +(num1) - +(num2);
 
 		$('#result').text(result)
 	})
