975730b09604f681b37260392a401daba7dc82c1
Bill Davis <bill.davis@vertical-knowledge.com>
2018-03-14 21:07:28 -0400

    Fixed Joel's garbage JS

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ $(document).ready(function() {
 	$('#plus').on('click', function() {
 		var num1 = $('#num1').val();
 		var num2 = $('#num2').val();
-		var result = num1 + num2;
-		console.log('plus')
-		console.log(result)
+		var result = parseInt(num1) + parseInt(num2);
 		$('#result').text(result)
 	});
 	
 	$('#minus').on('click', function() {
 		var num1 = $('#num1').val();
 		var num2 = $('#num2').val();
-		var result = num1 - num2;
+		var result = parseInt(num1) - parseInt(num2);
 
 		$('#result').text(result)
 	})
